dfea42ff60c5f8c0c1ac962d0368761afbb04538
cagoooo <ipad@mail2.smes.tyc.edu.tw>
2026-08-17 09:09:03 +0800

    🔗 更新阿凱老師石門國小索引超連結至新版校網網址

--- client/src/components/site-footer.tsx
+++ client/src/components/site-footer.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "wouter";
 
 const SCHOOL_PAGE_URL =
-    "https://www.smes.tyc.edu.tw/modules/tadnews/page.php?ncsn=11&nsn=16#a5";
+    "https://www.smes.tyc.edu.tw/modules/school/index.php?department_id=2&zone_id=0&page_id=2&content_id=11&type=news&from_op=all_news#a5";
 
 /**
  * 共用頁尾署名（阿凱老師）。
